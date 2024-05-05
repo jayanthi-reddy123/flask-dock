63232a8c89d6a2f383a16c22e000e48d99bdbfac
jayanthi-reddy123 <112416706+jayanthi-reddy123@users.noreply.github.com>
2024-05-05 10:34:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    # Run the app on port 6000
-    app.run(debug=True, host='0.0.0.0', port=6000)
+    # Run the app on port 8080
+    app.run(debug=True, host='0.0.0.0', port=8080)
